f9159057d008f91afcab76304f7c4e7b15aef929
tommyyangdev <tommyyang.dev@gmail.com>
2025-09-21 19:42:43 +1000

    Added controller routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 1234;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
