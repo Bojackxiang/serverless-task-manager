c9eaf74106b82dfed1c95f171f7498cc1d02e5dc
tommyyangdev <tommyyang.dev@gmail.com>
2025-09-21 19:53:01 +1000

    Hotfix PORT and env

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 1234;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 1234;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
